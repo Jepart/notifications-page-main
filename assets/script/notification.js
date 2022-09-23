84b1a68bb4231a77ec1ceca1a5c97ea0057ecb86
Jean Carlos <jean-los@hotmail.com>
2022-09-23 17:23:27 -0300

    .

---
+++ assets/script/notification.js
@@ -0,0 +1,58 @@
+notification = [
+{
+    user: "Mark Webber",
+    text: "reacted to your recent post",
+    action: "link",
+    result: "My first tournament today!",
+    time: "1m ago",
+    read: true
+},
+{
+    user: "Angela Gray",
+    text: "followed you",
+    action: false,
+    result: false,
+    time: "5m ago",
+    read: true
+},
+{
+    user: "Jacob Thompson",
+    text: "has joined your group",
+    action: "link_group",
+    result: "Chess Club",
+    time: "1 day ago",
+    read: true
+},
+{
+    user: "Rizky Hasanuddin",
+    text: "sent you a private message",
+    action: "message",
+    result: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
+    time: "5 days ago",
+    read: false
+},
+{
+    user: "Kimberly Smith",
+    text: "commented on your picture",
+    action: "image",
+    result: "assets/images/image-chess.webp",
+    time: "1 week ago",
+    read: false
+},
+{
+    user: "Nathan Peterson",
+    text: "reacted to your recent post",
+    action: "strong",
+    result: "5 end-game strategies to increase your win rate",
+    time: "2 weeks ago",
+    read: false
+},
+{
+    user: "Anna Kim",
+    text: "left the group",
+    action: "group",
+    result: "Chess Club",
+    time: "2 weeks ago",
+    read: false
+},
+]
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,60 +1,3 @@
-notification = [{
-    user: "Mark Webber",
-    text: "reacted to your recent post",
-    action: "link",
-    result: "My first tournament today!",
-    time: "1m ago",
-    read: true
-},
-{
-    user: "Angela Gray",
-    text: "followed you",
-    action: false,
-    result: false,
-    time: "5m ago",
-    read: true
-},
-{
-    user: "Jacob Thompson",
-    text: "has joined your group",
-    action: "link_group",
-    result: "Chess Club",
-    time: "1 day ago",
-    read: true
-},
-{
-    user: "Rizky Hasanuddin",
-    text: "sent you a private message",
-    action: "message",
-    result: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
-    time: "5 days ago",
-    read: false
-},
-{
-    user: "Kimberly Smith",
-    text: "commented on your picture",
-    action: "image",
-    result: "assets/images/image-chess.webp",
-    time: "1 week ago",
-    read: false
-},
-{
-    user: "Nathan Peterson",
-    text: "reacted to your recent post",
-    action: "strong",
-    result: "5 end-game strategies to increase your win rate",
-    time: "2 weeks ago",
-    read: false
-},
-{
-    user: "Anna Kim",
-    text: "left the group",
-    action: "group",
-    result: "Chess Club",
-    time: "2 weeks ago",
-    read: false
-},
-]
 let cRead = 0
 notification.forEach(e => {
     let action = ""
@@ -80,14 +23,10 @@ notification.forEach(e => {
 })
 $(".notification").append(`<span>${cRead}</span>`)
 const read_all = () => {
-notification.forEach(e => {
-    if (e.read) c = e.read = false
-})
-$(".notification > span").fadeOut(300, () => {
-    this.remove()
-})
-$("strong.dot").fadeOut(300, () => {
-    this.remove()
-})
-$('section > ul > li[read="true"]').attr('read', false)
+    notification.forEach(e => {
+        if (e.read) c = e.read = false
+    })
+    $(".notification > span").text(0)
+    $("strong.dot").fadeOut(300)
+    $('section > ul > li[read="true"]').attr('read', false)
 }
